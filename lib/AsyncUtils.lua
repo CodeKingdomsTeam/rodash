8d3465955a9c94c50c74d81dcbc113362d8ab67d
Hugh Collins <thehugheth@gmail.com>
2019-02-18 12:35:26 +0000

    AsyncUtils

---
+++ lib/AsyncUtils.lua
@@ -0,0 +1,65 @@
+local TableUtils = require(script.Parent.TableUtils)
+local AsyncUtils = {}
+
+local baseRandomStream = Random.new()
+
+function AsyncUtils.wrapAsync(fn)
+	return Promise.new(
+		function(resolve, reject)
+			coroutine.wrap(
+				function()
+					local ok, result = pcall(fn)
+					if ok then
+						resolve(result)
+					else
+						reject(result)
+					end
+				end
+			)()
+		end
+	)
+end
+
+function AsyncUtils.retryWithBackoff(getPromise, backoffOptions)
+	backoffOptions =
+		TableUtils.Assign(
+		{
+			maxTries = 3,
+			attemptNumber = 0,
+			retryPeriodInSeconds = 5,
+			initialDelayInSeconds = 2,
+			randomStream = baseRandomStream,
+			handleRetry = function()
+			end
+		},
+		backoffOptions
+	)
+	assert(backoffOptions.maxTries > 0, "You must try a function at least once")
+	local response = getPromise()
+	if backoffOptions.maxTries == 1 then
+		return response
+	else
+		return response:catch(
+			function()
+				local waitTime =
+					(backoffOptions.retryPeriodInSeconds ^ backoffOptions.attemptNumber) * backoffOptions.randomStream:NextNumber() +
+					backoffOptions.initialDelayInSeconds
+				backoffOptions.handleRetry(waitTime)
+				wait(waitTime)
+				AsyncUtils.tryWithBackoff(
+					getPromise,
+					TableUtils.Assign(
+						{},
+						backoffOptions,
+						{
+							maxTries = backoffOptions.maxTries - 1,
+							attemptNumber = backoffOptions.attemptNumber + 1
+						}
+					)
+				)
+			end
+		)
+	end
+end
+
+return AsyncUtils
