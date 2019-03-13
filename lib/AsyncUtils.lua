11a3b33a89dd1c48d0cd40baf96758d8207eb650
Hugh Collins <thehugheth@gmail.com>
2019-03-13 11:23:48 +0000

    Update formatting

--- lib/AsyncUtils.lua
+++ lib/AsyncUtils.lua
@@ -15,35 +15,35 @@ local baseRandomStream = Random.new()
     The promise resolves to an array mapping the input to resolved elements.
 ]]
 function AsyncUtils.parallel(things)
-    local promises =
-        TableUtils.Map(
-        things,
-        function(thing)
-            if Promise.is(thing) then
-                return thing
-            else
-                return Promise.resolve(thing)
-            end
-        end
-    )
-    return Promise.all(promises)
+	local promises =
+		TableUtils.Map(
+		things,
+		function(thing)
+			if Promise.is(thing) then
+				return thing
+			else
+				return Promise.resolve(thing)
+			end
+		end
+	)
+	return Promise.all(promises)
 end
 
 --[[
     Returns a promise which resolves after the given delayInSeconds.
 ]]
 function AsyncUtils.delay(delayInSeconds)
-    assert(type(delayInSeconds) == "number")
-    return Promise.new(
-        function(resolve)
-            delay(
-                delayInSeconds,
-                function()
-                    resolve()
-                end
-            )
-        end
-    )
+	assert(type(delayInSeconds) == "number")
+	return Promise.new(
+		function(resolve)
+			delay(
+				delayInSeconds,
+				function()
+					resolve()
+				end
+			)
+		end
+	)
 end
 
 --[[
@@ -52,21 +52,21 @@ end
     after any asynchronous actions, and rejects if the function throws an error.
 ]]
 function AsyncUtils.wrapAsync(fn)
-    assert(type(fn) == "function")
-    return Promise.new(
-        function(resolve, reject)
-            coroutine.wrap(
-                function()
-                    local ok, result = pcall(fn)
-                    if ok then
-                        resolve(result)
-                    else
-                        reject(result)
-                    end
-                end
-            )()
-        end
-    )
+	assert(type(fn) == "function")
+	return Promise.new(
+		function(resolve, reject)
+			coroutine.wrap(
+				function()
+					local ok, result = pcall(fn)
+					if ok then
+						resolve(result)
+					else
+						reject(result)
+					end
+				end
+			)()
+		end
+	)
 end
 
 --[[
@@ -85,89 +85,88 @@ end
     onFail(errorMessage) - a hook for when the promise has failed and no more retries are allowed
 ]]
 function AsyncUtils.retryWithBackoff(getPromise, backoffOptions)
-    assert(type(getPromise) == "function")
-    local function backoffThenRetry(errorMessage)
-        local waitTime =
-            (backoffOptions.retryExponentInSeconds ^ backoffOptions.attemptNumber) *
-            backoffOptions.randomStream:NextNumber() +
-            backoffOptions.retryConstantInSeconds
-        backoffOptions.onRetry(waitTime, errorMessage)
-        return AsyncUtils.delay(waitTime):andThen(
-            function()
-                return AsyncUtils.retryWithBackoff(
-                    getPromise,
-                    TableUtils.Assign(
-                        {},
-                        backoffOptions,
-                        {
-                            maxTries = backoffOptions.maxTries - 1,
-                            attemptNumber = backoffOptions.attemptNumber + 1
-                        }
-                    )
-                )
-            end
-        )
-    end
+	assert(type(getPromise) == "function")
+	local function backoffThenRetry(errorMessage)
+		local waitTime =
+			(backoffOptions.retryExponentInSeconds ^ backoffOptions.attemptNumber) * backoffOptions.randomStream:NextNumber() +
+			backoffOptions.retryConstantInSeconds
+		backoffOptions.onRetry(waitTime, errorMessage)
+		return AsyncUtils.delay(waitTime):andThen(
+			function()
+				return AsyncUtils.retryWithBackoff(
+					getPromise,
+					TableUtils.Assign(
+						{},
+						backoffOptions,
+						{
+							maxTries = backoffOptions.maxTries - 1,
+							attemptNumber = backoffOptions.attemptNumber + 1
+						}
+					)
+				)
+			end
+		)
+	end
 
-    local function getDurationMs()
-        return math.floor((tick() - backoffOptions.startTime) * 1000)
-    end
+	local function getDurationMs()
+		return math.floor((tick() - backoffOptions.startTime) * 1000)
+	end
 
-    backoffOptions =
-        TableUtils.Assign(
-        {
-            startTime = tick(),
-            maxTries = 5,
-            attemptNumber = 0,
-            retryExponentInSeconds = 5,
-            retryConstantInSeconds = 2,
-            randomStream = baseRandomStream,
-            onRetry = function()
-            end,
-            onDone = function()
-            end,
-            onFail = function()
-            end
-        },
-        backoffOptions
-    )
-    assert(backoffOptions.maxTries > 0, "You must try a function at least once")
-    local ok, response =
-        pcall(
-        function()
-            return getPromise()
-        end
-    )
-    if not ok then
-        if backoffOptions.maxTries == 1 then
-            backoffOptions.onFail(response)
-            return Promise.reject(response)
-        else
-            return backoffThenRetry(response)
-        end
-    elseif not Promise.is(response) then
-        backoffOptions.onDone(response, getDurationMs())
-        return Promise.resolve(response)
-    elseif backoffOptions.maxTries == 1 then
-        return response:andThen(
-            function(response)
-                backoffOptions.onDone(response, getDurationMs())
-                return response
-            end
-        ):catch(
-            function(message)
-                backoffOptions.onFail(message)
-                error(message)
-            end
-        )
-    else
-        return response:andThen(
-            function(response)
-                backoffOptions.onDone(response, getDurationMs())
-                return response
-            end
-        ):catch(backoffThenRetry)
-    end
+	backoffOptions =
+		TableUtils.Assign(
+		{
+			startTime = tick(),
+			maxTries = 5,
+			attemptNumber = 0,
+			retryExponentInSeconds = 5,
+			retryConstantInSeconds = 2,
+			randomStream = baseRandomStream,
+			onRetry = function()
+			end,
+			onDone = function()
+			end,
+			onFail = function()
+			end
+		},
+		backoffOptions
+	)
+	assert(backoffOptions.maxTries > 0, "You must try a function at least once")
+	local ok, response =
+		pcall(
+		function()
+			return getPromise()
+		end
+	)
+	if not ok then
+		if backoffOptions.maxTries == 1 then
+			backoffOptions.onFail(response)
+			return Promise.reject(response)
+		else
+			return backoffThenRetry(response)
+		end
+	elseif not Promise.is(response) then
+		backoffOptions.onDone(response, getDurationMs())
+		return Promise.resolve(response)
+	elseif backoffOptions.maxTries == 1 then
+		return response:andThen(
+			function(response)
+				backoffOptions.onDone(response, getDurationMs())
+				return response
+			end
+		):catch(
+			function(message)
+				backoffOptions.onFail(message)
+				error(message)
+			end
+		)
+	else
+		return response:andThen(
+			function(response)
+				backoffOptions.onDone(response, getDurationMs())
+				return response
+			end
+		):catch(backoffThenRetry)
+	end
 end
 
 return AsyncUtils
