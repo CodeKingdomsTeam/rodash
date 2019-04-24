6ed8e83e712610c30510feb252eea2273c5f76b2
Hugheth <thehugheth@gmail.com>
2019-04-24 12:51:00 +0100

    Add connectToStore

---
+++ lib/connectToStore.lua
@@ -0,0 +1,69 @@
+local tea = require(script.Parent.Parent.tea)
+local TableUtils = require(script.Parent.TableUtils)
+
+local function connectToStore(Class, mapStateToFields)
+	local ConnectedClass =
+		Class:extendWithInterface(
+		"Connected(" .. Class.name .. ")",
+		{
+			store = tea.interface(
+				{
+					changed = tea.Signal
+				}
+			)
+		}
+	)
+
+	function ConnectedClass:listen()
+		self._connection =
+			self._store.changed:Connect(
+			function(state)
+				local nextFields = mapStateToFields(state)
+				if self:shouldUpdate(nextFields) then
+					self:willUpdate(nextFields)
+				end
+				self._fields = nextFields
+				TableUtils.assign(self, nextFields)
+			end
+		)
+		if Class.listen then
+			Class:listen(self)
+		end
+		local nextFields = mapStateToFields(self._store:getState())
+		self._fields = nextFields
+		TableUtils.assign(self, nextFields)
+		if Class.didMount then
+			Class:didMount(self)
+		end
+	end
+
+	function ConnectedClass:shouldUpdate(nextFields)
+		if Class.shouldUpdate then
+			return Class.shouldUpdate(self, nextFields)
+		end
+		return not TableUtils.shallowEqual(self._fields, nextFields)
+	end
+
+	function ConnectedClass:didMount()
+		if Class.didMount then
+			Class.didMount(self)
+		end
+	end
+
+	function ConnectedClass:willUpdate(nextFields)
+		if Class.willUpdate then
+			Class.willUpdate(self, nextFields)
+		end
+	end
+
+	function ConnectedClass:destroy()
+		self._connection:Disconnect()
+		if Class.destroy then
+			Class.destroy(self)
+		end
+	end
+
+	return ConnectedClass
+end
+
+return connectToStore
---
+++ spec/connectToStore_spec.lua
@@ -0,0 +1,327 @@
+local connectToStore = require "connectToStore"
+local ClassUtils = require "ClassUtils"
+local tea = require "tea"
+
+local function getStoreStub(initialState)
+	local state = initialState
+	local callback
+	return {
+		getState = function()
+			return state
+		end,
+		changed = {
+			Connect = function(connection, handler)
+				callback = handler
+				return {
+					Disconnect = function()
+						callback = nil
+					end
+				}
+			end
+		},
+		setState = function(newState)
+			state = newState
+			if callback then
+				callback(newState)
+			end
+		end
+	}
+end
+
+describe(
+	"connectToStore",
+	function()
+		it(
+			"maps fields and calls didMount",
+			function()
+				local MyClass =
+					ClassUtils.makeClassWithInterface(
+					"Simple",
+					{
+						amount = tea.number
+					}
+				)
+				MyClass.didMount =
+					spy.new(
+					function()
+					end
+				)
+				local MyConnectedClass =
+					connectToStore(
+					MyClass,
+					function(state)
+						return {_field = state.field}
+					end
+				)
+				local store =
+					getStoreStub(
+					{
+						field = 5
+					}
+				)
+				local myInstance =
+					MyConnectedClass.new(
+					{
+						store = store,
+						amount = 10
+					}
+				)
+				myInstance:listen()
+				assert.spy(MyClass.didMount).was_called()
+				assert.equals(5, myInstance._field)
+			end
+		)
+		it(
+			"complains about invalid fields",
+			function()
+				local MyClass =
+					ClassUtils.makeClassWithInterface(
+					"Simple",
+					{
+						amount = tea.number
+					}
+				)
+				local MyConnectedClass = connectToStore(MyClass)
+				assert.errors(
+					function()
+						MyConnectedClass.new(
+							{
+								amount = 10
+							}
+						)
+					end,
+					[[Class Connected(Simple) cannot be instantiated
+[interface] bad value for store:
+	table expected, got nil]]
+				)
+				assert.errors(
+					function()
+						MyConnectedClass.new(
+							{
+								store = getStoreStub(),
+								amount = "what"
+							}
+						)
+					end,
+					[[Class Connected(Simple) cannot be instantiated
+[interface] bad value for amount:
+	number expected, got string]]
+				)
+			end
+		)
+		it(
+			"calls willUpdate during state change",
+			function()
+				local MyClass =
+					ClassUtils.makeClassWithInterface(
+					"Simple",
+					{
+						amount = tea.number
+					}
+				)
+				MyClass.willUpdate =
+					spy.new(
+					function()
+					end
+				)
+				local MyConnectedClass =
+					connectToStore(
+					MyClass,
+					function(state)
+						return {_field = state.field}
+					end
+				)
+				local store =
+					getStoreStub(
+					{
+						field = 5
+					}
+				)
+				local myInstance =
+					MyConnectedClass.new(
+					{
+						store = store,
+						amount = 10
+					}
+				)
+				myInstance:listen()
+				assert.spy(MyClass.willUpdate).was_not_called()
+				store.setState(
+					{
+						field = 6
+					}
+				)
+				assert.spy(MyClass.willUpdate).was_called()
+				assert.equals(5, MyClass.willUpdate.calls[1].vals[1]._field)
+				assert.equals(6, MyClass.willUpdate.calls[1].vals[2]._field)
+				assert.equals(6, myInstance._field)
+			end
+		)
+		it(
+			"does not call willUpdate if state is shallow equal",
+			function()
+				local MyClass =
+					ClassUtils.makeClassWithInterface(
+					"Simple",
+					{
+						amount = tea.number
+					}
+				)
+				MyClass.willUpdate =
+					spy.new(
+					function()
+					end
+				)
+				local MyConnectedClass =
+					connectToStore(
+					MyClass,
+					function(state)
+						return {_field = state.field}
+					end
+				)
+				local store =
+					getStoreStub(
+					{
+						field = 5
+					}
+				)
+				local myInstance =
+					MyConnectedClass.new(
+					{
+						store = store,
+						amount = 10
+					}
+				)
+				myInstance:listen()
+				assert.spy(MyClass.willUpdate).was_not_called()
+				store.setState(
+					{
+						field = 5
+					}
+				)
+				assert.spy(MyClass.willUpdate).was_not_called()
+				store.setState(
+					{
+						field = 6
+					}
+				)
+				assert.spy(MyClass.willUpdate).was_called()
+			end
+		)
+		it(
+			"does not call willUpdate if shouldUpdate returns false, but ensures fields are consistent",
+			function()
+				local MyClass =
+					ClassUtils.makeClassWithInterface(
+					"Simple",
+					{
+						amount = tea.number
+					}
+				)
+				MyClass.willUpdate =
+					spy.new(
+					function()
+					end
+				)
+				MyClass.shouldUpdate =
+					spy.new(
+					function()
+						return false
+					end
+				)
+				local MyConnectedClass =
+					connectToStore(
+					MyClass,
+					function(state)
+						return {_field = state.field}
+					end
+				)
+				local store =
+					getStoreStub(
+					{
+						field = 5
+					}
+				)
+				local myInstance =
+					MyConnectedClass.new(
+					{
+						store = store,
+						amount = 10
+					}
+				)
+				myInstance:listen()
+				assert.spy(MyClass.willUpdate).was_not_called()
+				assert.spy(MyClass.shouldUpdate).was_not_called()
+				store.setState(
+					{
+						field = 12
+					}
+				)
+				assert.spy(MyClass.willUpdate).was_not_called()
+				assert.spy(MyClass.shouldUpdate).was_called()
+				assert.equals(12, myInstance._field)
+			end
+		)
+		it(
+			"does not update fields after instance is destroyed",
+			function()
+				local MyClass =
+					ClassUtils.makeClassWithInterface(
+					"Simple",
+					{
+						amount = tea.number
+					}
+				)
+				MyClass.willUpdate =
+					spy.new(
+					function()
+					end
+				)
+				MyClass.shouldUpdate =
+					spy.new(
+					function()
+						return true
+					end
+				)
+				MyClass.destroy =
+					spy.new(
+					function()
+					end
+				)
+				local MyConnectedClass =
+					connectToStore(
+					MyClass,
+					function(state)
+						return {_field = state.field}
+					end
+				)
+				local store =
+					getStoreStub(
+					{
+						field = 5
+					}
+				)
+				local myInstance =
+					MyConnectedClass.new(
+					{
+						store = store,
+						amount = 10
+					}
+				)
+				myInstance:listen()
+				assert.spy(MyClass.willUpdate).was_not_called()
+				assert.spy(MyClass.shouldUpdate).was_not_called()
+				myInstance:destroy()
+				assert.spy(MyClass.destroy).was_called()
+				store.setState(
+					{
+						field = 12
+					}
+				)
+				assert.spy(MyClass.willUpdate).was_not_called()
+				assert.spy(MyClass.shouldUpdate).was_not_called()
+				assert.equals(5, myInstance._field)
+			end
+		)
+	end
+)
